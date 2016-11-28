9d0a54a6a3d5f5233c5d6d4e6a02c46b6e4939f1
Ian Hollands <ihollands@outlook.com>
2016-11-28 11:11:58 -0500

    read of hardcoded data

---
+++ app.js
@@ -0,0 +1,39 @@
+
+angular
+  .module("weightMateApp", [])
+  .controller("buddiesCtrl", [buddyControllerFunction])
+
+
+    function buddyControllerFunction() {
+      this.buddies = buddyData
+    }
+
+
+  let buddyData = [
+    { username: 'ihollands',
+      first_name: 'Ian',
+      last_name: 'Hollands',
+      img_url: 'http://lorempixel.com/200/200/people',
+      email: 'ianchollands@gmail.com',
+      city: 'Washington',
+      state: 'DC',
+      zip: '20010',
+      street_address: '1312 Park Rd NW',
+      bench: 255,
+      squat: 315,
+      deadlift: 405
+    },
+    { username: 'dvanvleezy',
+      first_name: 'Diana',
+      last_name: 'Van Vleet',
+      img_url: 'http://lorempixel.com/200/200/people',
+      email: 'dvanvleet88@gmail.com',
+      city: 'Alexandria',
+      state: 'VA',
+      zip: '22314',
+      street_address: '30 Wolfe St',
+      bench: 100,
+      squat: 135,
+      deadlift: 135
+    }
+  ]
